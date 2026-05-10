c88d6209fe61593fe59efe7ea0135727770e9c48
ocasta181 <ocasta181@gmail.com>
2026-05-10 00:11:58 -0600

    Add session state machine

--- domain/session/types.ts
+++ domain/session/types.ts
@@ -24,3 +24,53 @@ export type SessionAggregate = Omit<
   assessment: Assessment;
   stimulationSets: StimulationSet[];
 };
+
+export type SessionFlowState =
+  | "idle"
+  | "target_selection"
+  | "preparation"
+  | "stimulation"
+  | "interjection"
+  | "closure"
+  | "review"
+  | "post_session";
+
+export type SessionFlowAction =
+  | "start_session"
+  | "select_target"
+  | "create_target_draft"
+  | "update_assessment"
+  | "approve_assessment"
+  | "start_stimulation"
+  | "pause_stimulation"
+  | "log_stimulation_set"
+  | "continue_stimulation"
+  | "request_grounding"
+  | "begin_closure"
+  | "review_session"
+  | "complete_session"
+  | "return_to_idle";
+
+export type AgentToolName =
+  | "create_target_draft"
+  | "select_target"
+  | "update_session_assessment"
+  | "start_stimulation"
+  | "pause_stimulation"
+  | "log_stimulation_set"
+  | "close_session"
+  | "show_grounding_prompt"
+  | "request_user_review";
+
+export type SessionFlowTransition = {
+  action: SessionFlowAction;
+  nextState: SessionFlowState;
+};
+
+export type SessionFlowStateDefinition = {
+  state: SessionFlowState;
+  label: string;
+  description: string;
+  transitions: SessionFlowTransition[];
+  allowedAgentTools: AgentToolName[];
+};
