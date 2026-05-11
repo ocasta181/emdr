8355843e6508b64edc24cb900acd322e4100584b
ocasta181 <ocasta181@gmail.com>
2026-05-10 21:50:19 -0600

    Remove old UI shell

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,5 @@
 import { createRoot } from "react-dom/client";
 import "./styles.css";
-import { App } from "../domain/app/components/App";
 import { AnimatedApp } from "../domain/app/components/AnimatedApp";
 
-const uiMode = new URLSearchParams(window.location.search).get("ui");
-
-createRoot(document.getElementById("root")!).render(uiMode === "animated" ? <AnimatedApp /> : <App />);
+createRoot(document.getElementById("root")!).render(<AnimatedApp />);
